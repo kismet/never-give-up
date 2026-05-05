35ea3e1ca74f9e13b311ad46171b08da5fda836b
SALATTI DARIO <salatti.dario@dominio.local>
2026-05-05 09:48:17 +0200

    modifica codice

--- sources/libs.cpp
+++ sources/libs.cpp
@@ -1,3 +1,10 @@
 #include "../includes/types.h"
 #include "../includes/libs.h"
 
+// Autore: (Salatti Dario)
+// Data:   (2008/05/05)
+// Classe: 3CI
+// Fila:    (B)
+
+// Inizializzazione comoda della struct se l'inizializzazione non è possibile restituisce false
+bool crea_traccia(Traccia_t& t, uint16_t o, uint8_t m, uint8_t s, uint32_t ms);
