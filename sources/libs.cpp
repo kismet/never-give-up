fa15b774c6aa33524dd0bf966dfd6001d08efc0e
pandolfimarco <pandolfimarco816@gmail.com>
2026-05-05 09:32:39 +0200

    writed the function print and fixed some bugs in function diff

--- sources/libs.cpp
+++ sources/libs.cpp
@@ -1,6 +1,9 @@
 #include "../includes/types.h"
 #include "../includes/libs.h"
 
+#include <iostream>
+#include <ostream>
+
 bool create(Tempo_t& t, uint16_t o, uint8_t m, uint8_t s, uint32_t ms) {
     Tempo_t tempo;
     tempo.ore=o;
@@ -64,15 +67,19 @@ void add(Tempo_t& tempo1, Tempo_t& tempo2) {
 }
 
 bool diff(Tempo_t& tempo1, const Tempo_t* tempo2) {
-    if (after(tempo1, tempo2)) {
-        tempo1.ore=tempo1.ore-tempo2.ore;
-        tempo1.minuti=tempo1.minuti-tempo2.minuti;
-        tempo1.secondi=tempo1.secondi-tempo2.secondi;
-        tempo1.microsecondi=tempo1.microsecondi-tempo2.microsecondi;
+    if (after(tempo1, *tempo2)) {
+        tempo1.ore=tempo1.ore-tempo2->ore;
+        tempo1.minuti=tempo1.minuti-tempo2->minuti;
+        tempo1.secondi=tempo1.secondi-tempo2->secondi;
+        tempo1.microsecondi=tempo1.microsecondi-tempo2->microsecondi;
 
         return true;
     }
     else {
         return false;
     }
+}
+
+void print(Tempo_t* tempo) {
+    std::cout<<tempo->ore<<":"<<tempo->minuti<<":"<<tempo->secondi<<":"<<tempo->microsecondi<<std::endl;
 }
