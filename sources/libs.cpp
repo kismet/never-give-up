a41c81e17318aea5b6a71587b3f1d7efeec47830
ESPOSITO LORENZO <esposito.lorenzo@dominio.local>
2026-05-05 08:58:09 +0200

    implemented functions "minor_autonomia" and "stampa_autonomia"

--- sources/libs.cpp
+++ sources/libs.cpp
@@ -1,3 +1,32 @@
+// Autore: (Esposito Lorenzo)
+// Data:   (2026/05/05)
+// Classe: 3CI
+// Fila:    (Fila D)
+
 #include "../includes/types.h"
 #include "../includes/libs.h"
 
+#include <iostream>
+#include <ostream>
+using namespace std;
+
+bool minor_autonomia(const Batteria_t& c1, const Batteria_t& c2) {
+    if(c1.hours < c2.hours) {
+        return true;
+    }
+    else if(c1.minutes < c2.minutes) {
+        return true;
+    }
+    else if(c1.seconds < c2.seconds) {
+        return true;
+    }
+    else if(c1.microseconds < c2.microseconds) {
+        return true;
+    }
+
+    return false;
+}
+
+void stampa_autonomia(Batteria_t* c) {
+    cout << c->hours << ":" << c->minutes << ":" << c->seconds << "." << c->microseconds << endl;
+}
