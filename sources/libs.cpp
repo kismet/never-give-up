18b33546e29b1d9c16f351c27f23bcb5099a5ac6
eliaturini2009 <eliaturini2009@gmail.com>
2026-05-05 08:56:21 +0200

    creata la funzione setup

--- sources/libs.cpp
+++ sources/libs.cpp
@@ -1,3 +1,25 @@
 #include "../includes/types.h"
 #include "../includes/libs.h"
 
+bool setup(Batteria_t& c, uint16_t o, uint8_t m, uint8_t s, uint32_t ms) {
+    bool inizializzazioneCorretta = true;
+
+    c.hours = o;
+    if (c.hours == NULL) {
+        inizializzazioneCorretta = false;
+    }
+    c.minutes = m;
+    if (c.minutes == NULL) {
+        inizializzazioneCorretta = false;
+    }
+    c.seconds = s;
+    if (c.seconds == NULL) {
+        inizializzazioneCorretta = false;
+    }
+    c.microseconds = ms;
+    if (c.microseconds == NULL) {
+        inizializzazioneCorretta = false;
+    }
+
+    return inizializzazioneCorretta;
+}
