4164d1691e0fb2fcafd6aef3e830ee9ca215d85d
eliaturini2009 <eliaturini2009@gmail.com>
2026-05-05 09:31:04 +0200

    aggiunti include mancanti in libs.h

--- includes/libs.h
+++ includes/libs.h
@@ -1,6 +1,9 @@
 #ifndef NEVER_GIVE_UP_LIBS_H
 #define NEVER_GIVE_UP_LIBS_H
 
+#include <cstdint>
+#include "types.h"
+
 // Inizializzazione comoda della struct se l'inizializzazione non è possibile restituisce false
 bool setup(Batteria_t& c, uint16_t o, uint8_t m, uint8_t s, uint32_t ms);
 
