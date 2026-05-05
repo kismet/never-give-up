4da0064f5b3bb35a40aa930f518040b80b3e7ae6
ESPOSITO LORENZO <esposito.lorenzo@dominio.local>
2026-05-05 09:21:23 +0200

    implemented function "scarica"

--- sources/libs.cpp
+++ sources/libs.cpp
@@ -54,3 +54,45 @@ void ricarica(Batteria_t& c1, Batteria_t& c2) {
     c1.seconds += c2.seconds;
     c1.microseconds += c2.microseconds;
 }
+
+bool scarica(Batteria_t& c1, Batteria_t* c2) {
+    Batteria_t copia1;
+    copia1.hours = c1.hours;
+    copia1.minutes = c1.minutes;
+    copia1.seconds = c1.seconds;
+    copia1.microseconds = c1.microseconds;
+
+    if(c1.hours-c2->hours < 0) {
+        return false;
+    }
+    else {
+        c1.hours -= c2->hours;
+    }
+
+    if(c1.minutes-c2->minutes < 0) {
+        c1.hours = copia1.hours;
+        return false;
+    }
+    else {
+        c1.minutes -= c2->minutes;
+    }
+
+    if(c1.seconds-c2->seconds < 0) {
+        c1.hours = copia1.hours;
+        c1.minutes = copia1.minutes;
+        return false;
+    }
+    else {
+        c1.seconds -= c2->seconds;
+    }
+
+    if(c1.microseconds-c2->microseconds < 0) {
+        c1.hours = copia1.hours;
+        c1.minutes = copia1.minutes;
+        c1.seconds = copia1.seconds;
+        return false;
+    }
+    else {
+        c1.microseconds -= c2->microseconds;
+    }
+}
