3e8bf204185f4cd6d833f45d54ad4085f390a1c8
ESPOSITO LORENZO <esposito.lorenzo@dominio.local>
2026-05-05 09:03:25 +0200

    implemented functions "maggior_autonomia" and "ricarica"

--- sources/libs.cpp
+++ sources/libs.cpp
@@ -30,3 +30,27 @@ bool minor_autonomia(const Batteria_t& c1, const Batteria_t& c2) {
 void stampa_autonomia(Batteria_t* c) {
     cout << c->hours << ":" << c->minutes << ":" << c->seconds << "." << c->microseconds << endl;
 }
+
+bool maggior_autonomia(const Batteria_t& c1, const Batteria_t& c2) {
+    if(c1.hours < c2.hours) {
+        return false;
+    }
+    else if(c1.minutes < c2.minutes) {
+        return false;
+    }
+    else if(c1.seconds < c2.seconds) {
+        return false;
+    }
+    else if(c1.microseconds < c2.microseconds) {
+        return false;
+    }
+
+    return true;
+}
+
+void ricarica(Batteria_t& c1, Batteria_t& c2) {
+    c1.hours += c2.hours;
+    c1.minutes += c2.minutes;
+    c1.seconds += c2.seconds;
+    c1.microseconds += c2.microseconds;
+}
