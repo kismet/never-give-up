af35c4d76ccf9f08808c7bb28a4a47dfdce8e214
mannucci.marco <mannucim85@gmail.com>
2026-05-05 08:56:22 +0200

    Implemented stampa_autonomia e maggiore_autonomia

--- sources/libs.cpp
+++ sources/libs.cpp
@@ -42,3 +42,24 @@ bool minor_autonomia(const Batteria_t& c1, const Batteria_t& c2) {
 
     return false;
 }
+
+void stampa_autonomia(Batteria_t* c) {
+
+    cout << c->hours << ":" << c->minutes << ":" << c->seconds << ":" << c->microseconds << endl;
+}
+
+bool maggior_autonomia(const Batteria_t& c1, const Batteria_t& c2) {
+
+    if ( c1.hours > c2.hours ) {
+        if ( c1.minutes > c2.minutes ) {
+            if ( c1.seconds > c2.seconds ) {
+                if ( c1.microseconds > c2.microseconds ) {
+                    return true;
+                }
+            }
+        }
+    }
+
+    return false;
+    
+}
