bbb80d08055a3d82543bfb0a82b63d65e8d23592
stefano@lenzi.pro <stefano@lenzi.pro>
2026-05-14 00:00:49 +0200

    Correzione e valutazione capezuoli

--- sources/libs.cpp
+++ sources/libs.cpp
@@ -11,6 +11,7 @@ bool create(Tempo_t& t, uint16_t o, uint8_t m, uint8_t s, uint32_t ms) {
 
     int millisecondi;
 
+    //LENZI Servono 1000 microsecondi per fare un secondo
     while (t.microsecondi > 100) {
         t.microsecondi = t.microsecondi - 100;
         millisecondi++;
@@ -20,6 +21,7 @@ bool create(Tempo_t& t, uint16_t o, uint8_t m, uint8_t s, uint32_t ms) {
         millisecondi = millisecondi - 100;
         t.secondi++;
     }
+    //LENZI bastava fare secondi+=microsecondi/(1000*1000) e microsecondi=microsecondi%(1000*1000)
     while (t.secondi > 60) {
         t.secondi = t.secondi - 60;
         t.minuti++;
@@ -31,17 +33,22 @@ bool create(Tempo_t& t, uint16_t o, uint8_t m, uint8_t s, uint32_t ms) {
         m = t.minuti;
     }
     if (t.ore != o) {
+        //LENZI perchè?
         return false;
     }
     if (t.minuti != m) {
+        //LENZI perchè?
         return false;
     }
     if (t.secondi != s) {
+        //LENZI perchè?
         return false;
     }
     if (t.microsecondi != s) {
+        //LENZI perchè?
         return false;
     }
+    //LENZI manca return true
 }
 
 // True se tempo1 viene prima di tempo2
@@ -49,6 +56,13 @@ bool before(const Tempo_t& tempo1, const Tempo_t& tempo2) {
     if (tempo1.ore < tempo2.ore) {
         return true;
     }
+    //LENZI dovevi mettere questo
+    /*
+    else if ( tempo1.ore > tempo2.ore ){
+        return false;
+    }
+    */
+    //LENZI logica sbagliata in questo caso non è deto che tempo1.ore < tempo2.ore
     if (tempo1.minuti < tempo2.minuti) {
         return true;
     }
@@ -63,12 +77,14 @@ bool before(const Tempo_t& tempo1, const Tempo_t& tempo2) {
 
 // True se tempo1 viene dopo tempo2
 bool after(const Tempo_t& tempo1, const Tempo_t& tempo2) {
+    //LENZI sbagliata la logica, possono essere uguali se non è dopo!
      return !before(tempo1, tempo2);
 }
 
 // True se e solo se tempo1 e tempo2 sono uguali
 bool equal(const Tempo_t& tempo1, const Tempo_t& tempo2) {
     if (tempo1.ore == tempo2.ore) {
+        //LENZI sbagliata la logica, se sono diversi è falso, ma altrimenti devo vedere anche il resto delle unita
         return true;
     }
     if (tempo1.minuti == tempo2.minuti) {
@@ -85,6 +101,7 @@ bool equal(const Tempo_t& tempo1, const Tempo_t& tempo2) {
 
 // Somma i due tempi ed il risultato va in tempo1
 void add(Tempo_t& tempo1, Tempo_t& tempo2) {
+    //LENZI devi verificare che ore minuti e secondi siano tra 0 e 60 e fare il riporto
     tempo1.ore = tempo1.ore + tempo2.ore;
     tempo1.minuti = tempo1.minuti + tempo2.minuti;
     tempo1.secondi = tempo1.secondi + tempo2.secondi;
@@ -98,6 +115,7 @@ bool diff(Tempo_t& tempo1, const Tempo_t* tempo2) {
     temp.secondi = tempo1.secondi - tempo2->secondi;
     temp.microsecondi = tempo1.microsecondi - tempo2->microsecondi;
     if (temp.ore > 0) {
+        //LENZI sbagliata la logica se minore di zero puoi dire false ed uscire
         return true;
     }
     if (temp.minuti > 0) {
@@ -109,6 +127,7 @@ bool diff(Tempo_t& tempo1, const Tempo_t* tempo2) {
     if (temp.microsecondi > 0) {
         return true;
     }
+    //LENZI qui doveva essere vero e dovevi assegnare tempo1 = temp;
     return false;
 }
 
