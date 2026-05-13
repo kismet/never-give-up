69fc7ffba6760a30504195386ed1e193a389d079
stefano@lenzi.pro <stefano@lenzi.pro>
2026-05-14 01:10:42 +0200

    Correzione e valutazione Ciardelli

---
+++ RESULTS.md
@@ -0,0 +1,22 @@
+# Risultati
+## Uso di Git
+- Svolge correttamente
+  - Clone/Checkout
+  - Accetta permesso di scrittura sul server
+- Non svolge
+  - Commit locali
+  - Creazione del branch
+  - Push dei commit sul branch corretto
+  - Push del branch sul repository
+  - Merge del brach
+
+**Valutazione: 4/10**
+## Programmazione
+- [0/1]: Il programma non compila
+  - Errore nella gestione degli include
+- [0/5] Nessuna funzione corretta
+- [1/1] Coretti gli errori iniziali del programma
+- [0/1] Completo ma non corretto
+- [0/1] Uso corretto delle struct e puntatori
+
+**Valutazione: 2/10**
--- includes/libs.h
+++ includes/libs.h
@@ -1,6 +1,6 @@
 #ifndef NEVER_GIVE_UP_LIBS_H
 #define NEVER_GIVE_UP_LIBS_H
-
+//LENZI Questo è il file degli header andava implementato nel file libs.cpp
 // Autore: Alessandro Ciardelli
 // Data:   2026/05/05
 // Classe: 3CI
@@ -8,6 +8,7 @@
 
 // Inizializzazione comoda della struct se l'inizializzazione non è possibile restituisce false
 bool crea_traccia(Traccia_t& t, uint16_t o, uint8_t m, uint8_t s, uint32_t ms) {
+    //LENZI solo bozza
     if () {
         return true;
     }
@@ -16,12 +17,14 @@ bool crea_traccia(Traccia_t& t, uint16_t o, uint8_t m, uint8_t s, uint32_t ms) {
 
 // Somma la durata delle tracce ed il risultato va in traccia1
 void accoda(Traccia_t& traccia1, Traccia_t& traccia2) {
+    //LENZI grave ci sono i parametri da usare non va creato qui
     traccia1 = crea_traccia(traccia1 2, 3, 4, 5);
 }
 
 // Sottrae la durata di traccia2 da traccia1 se possibile e restituisce true altrimenti restituisce false e non effettua
 // la sottrazione. La sottrazione non è valida traccia1 diventa negativa
 bool taglia(Traccia_t& traccia1, const Traccia_t* traccia2) {
+    //LENZI grave non si usano così le strutture
     if (traccia1 >= traccia2) {
         traccia1 -= traccia2;
         return true;
@@ -31,21 +34,25 @@ bool taglia(Traccia_t& traccia1, const Traccia_t* traccia2) {
 
 // True se traccia1 è meno lunga di traccia2
 bool piu_breve(const Traccia_t& traccia1, const Traccia_t& traccia2) {
+    //LENZI grave non si usano così le strutture
     return traccia1 < traccia2;
 }
 
 // True se traccia1 è più lunga traccia2
 bool piu_lunga(const Traccia_t& traccia1, const Traccia_t& traccia2) {
+    //LENZI grave non si usano così le strutture
     return traccia1 > traccia2;
 }
 
 // True se e solo se traccia1 e traccia2 sono uguali
 bool stessa_durata(const Traccia_t& traccia1, const Traccia_t& traccia2) {
+    //LENZI grave non si usano così le strutture
     return traccia1 == traccia2;
 }
 
 // Stampa un traccia in modo formattato HHH:MM:SS.MS
 void stampa_info( Traccia_t* traccia) {
+    //LENZI grave non si usano così le strutture e i puntatori
     cout << *traccia << endl;
 }
 
