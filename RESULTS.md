8c9782a9c938264212b3f03cdcefeda77b0a0f76
stefano@lenzi.pro <stefano@lenzi.pro>
2026-05-14 00:56:30 +0200

    Correzione e valutazione Clipa

---
+++ RESULTS.md
@@ -0,0 +1,21 @@
+# Risultati
+## Uso di Git
+- Svolge correttamente
+  - Clone/Checkout
+  - Commit locali 
+- Non svolge
+  - Creazione del branch
+  - Push dei commit sul branch corretto
+  - Push del branch sul repository
+  - Merge del brach
+
+**Valutazione: 4/10**
+## Programmazione
+- [0/1]: Il programma non compila
+    - Errore nella gestione degli include
+- [0/5] Nessuna funzione corretta
+- [1/1] Coretti gli errori iniziali del programma
+- [0/1] Completo ma non corretto
+- [0/1] Uso corretto delle struct e puntatori
+
+**Valutazione: 2/10**
--- sources/libs.cpp
+++ sources/libs.cpp
@@ -9,7 +9,7 @@ bool create(Tempo_t& t, uint16_t o, uint8_t m, uint8_t s, uint32_t ms) {
 
 // True se tempo1 viene prima di tempo2
 bool before(const Tempo_t& tempo1, const Tempo_t& tempo2) {
-
+    //LENZI non si usano così le struct
     if (tempo1 < tempo2) {
         return true;
     }
@@ -18,6 +18,7 @@ bool before(const Tempo_t& tempo1, const Tempo_t& tempo2) {
 
 // True se tempo1 viene dopo tempo2
 bool after(const Tempo_t& tempo1, const Tempo_t& tempo2) {
+    //LENZI non si usano così le struct
     if (tempo1 > tempo2) {
         return true;
     }
@@ -25,6 +26,7 @@ bool after(const Tempo_t& tempo1, const Tempo_t& tempo2) {
 
 // True se e solo se tempo1 e tempo2 sono uguali
 bool equal(const Tempo_t& tempo1, const Tempo_t& tempo2) {
+    //LENZI non si usano così le struct
     if (tempo1 == tempo2) {
         return true;
     }
@@ -32,6 +34,7 @@ bool equal(const Tempo_t& tempo1, const Tempo_t& tempo2) {
 
 // Somma i due tempi ed il risultato va in tempo1
 void add(Tempo_t& tempo1, Tempo_t& tempo2) {
+    //LENZI non si usano così le struct
     tempo1 = tempo1 + tempo2;
 };
 
